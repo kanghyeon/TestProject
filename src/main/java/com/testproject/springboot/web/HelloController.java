5006471c4c2c088d182cd9ba5755f48db44727c0
kanghyoen kim <prove77@naver.com>
2020-12-17 21:41:40 +0900

    게시판 만들기 완료

---
+++ src/main/java/com/testproject/springboot/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.testproject.springboot.web;
+
+import com.testproject.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        System.out.println("test");
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+}
